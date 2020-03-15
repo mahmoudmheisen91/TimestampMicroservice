2f4295ce23400e791a5c57a35dbb7cd363839306
Glitch (mahmoudmheisen91-timestampmicroservice) <none>
2020-03-15 03:45:50 +0000

    🏹⚓️ Updated with Glitch

--- server.js
+++ server.js
@@ -4,6 +4,10 @@
 // init project
 var express = require("express");
 var app = express();
+// var bodyParser = require('body-parser');
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
@@ -30,6 +34,21 @@ app.get("/api/timestamp/:date_string?", (req, res) => {
   if (!date.getTime()) obj = { error: "Invalid Date" };
 
   res.json(obj);
+});
+
+app.use(express.urlencoded());
+app.post('/submit-form', (req, res) => {
+  let req_date = req.body.stamp;
+  if (+req_date) req_date = +req_date;
+
+  let date = new Date();
+  if (req_date) date = new Date(req_date);
+
+  let obj = { unix: date.getTime(), utc: date.toUTCString() };
+  if (!date.getTime()) obj = { error: "Invalid Date" };
+
+  res.json(obj);
+  res.end();
 });
 
 // listen for requests :)
